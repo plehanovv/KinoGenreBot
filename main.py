4c7ef095865de823f52466f0a749b41e752bfa7f
yarik <plehanov.kzn2@gmail.com>
2024-10-14 17:52:34 +0300

    Add func

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram import F
 from dotenv import load_dotenv
 import os
 
@@ -23,6 +24,11 @@ async def process_help_command(message: Message):
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
     )
+
+
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
 
 @dp.message()
